92a7c2111c77648f26cb79be71912fa36c079c5d
Александр <a.nasulchak@gmail.com>
2023-12-05 14:42:14 +0200

    Corrected in task 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,4 +1,4 @@
 // code
 let number1 = Math.floor(Math.random() * 100)
 
-console.log(number1, number1%2 ? ' :Это нечетное число' : ' :Это четное число')
+console.log(number1, number1 % 2 ? ' :Это нечетное число' : ' :Это четное число')
